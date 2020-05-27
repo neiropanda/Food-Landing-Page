db1ccdf69ee5d8de265b0dfa6437b11e4ab3bca2
Oleksandr <bekh.fa@gmail.com>
2020-05-28 02:00:26 +0300

    added rest operator for future css classes inside MmenuCard class

--- js/main.js
+++ js/main.js
@@ -144,12 +144,13 @@ document.addEventListener('DOMContentLoaded', () => {
   // Classes for cards
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
       this.changeToUAH();
@@ -161,8 +162,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     render() {
       const element = document.createElement('div');
+
+      if (this.classes.length === 0) {
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
       element.innerHTML = `
-      <div class="menu__item">
         <img src=${this.src} alt=${this.alt}>
         <h3 class="menu__item-subtitle">${this.title}</h3>
         <div class="menu__item-descr">${this.descr}</div>
@@ -171,7 +179,6 @@ document.addEventListener('DOMContentLoaded', () => {
           <div class="menu__item-cost">Цена:</div>
           <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
         </div>
-      </div>
       `;
       this.parent.append(element);
     }
@@ -184,7 +191,8 @@ document.addEventListener('DOMContentLoaded', () => {
     `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежиx
     овощей и фруктов. Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
     9,
-    '.menu .container'
+    '.menu .container',
+    'menu__item'
   ).render();
 
   new MenuCard(
@@ -195,7 +203,8 @@ document.addEventListener('DOMContentLoaded', () => {
     и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода
     в ресторан!`,
     15,
-    '.menu .container'
+    '.menu .container',
+    'menu__item'
   ).render();
 
   new MenuCard(
@@ -206,7 +215,8 @@ document.addEventListener('DOMContentLoaded', () => {
     продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное
     количество белков за счет тофу и импортных вегетарианских стейков.`,
     11,
-    '.menu .container'
+    '.menu .container',
+    'menu__item'
   ).render();
 
 });
